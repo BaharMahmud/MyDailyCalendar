39d2edab4fd48984bbff1e8c716520ce5818761d
BaharMahmud <bahar.a.mahmud@gmail.com>
2023-12-17 23:14:12 -0500

    code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,68 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
+
+
+
+
+
+$(function(){
+  console.log("Ready");
+});
+var today=dayjs();
+  $("#currentDay").text(today.format("MMM D,YYYY"));
+var currentHour=today.format("h");
+
+$(".time-block").each(function() {
+  var timeBlock=$(this).attr("id").split("-")[1];
+
+if (timeBlock==currentHour){
+  $(this).addClass("present");
+  $(this).children(".description").addClass("present");
+
+}
+
+
+else if(timeBlock<currentHour){
+  $(this).removeClass("present");
+  $(this).addClass("past");
+}
+
+
+
+
+
+else if( timeBlock>currentHour){
+  $(this).removeClass("present");
+  $(this).addClass("future");
+}
+});
+
+$(".saveBtn").on("click",function(event){
+event.preventDefault();
+var value=$(this).siblings(".description").val();
+var time=$(this).parent().attr("id").split("-")[1];
+localStorage.setItem(time,value);
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +81,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
